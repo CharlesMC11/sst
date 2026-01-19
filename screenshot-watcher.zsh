cb6444a0e36cacac72be89b39f991d7135bdbb20
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-01-19 12:20:41 -0800

    `Trap` and `mkdir` in one line

--- screenshot-watcher.zsh
+++ screenshot-watcher.zsh
@@ -23,15 +23,11 @@ path=(
 
 ################################################################################
 
-if ! mkdir "$LOCK" 2>/dev/null; then
-    echo 'Lock exists; script already in progress...' >&2
-    exit 1
-fi
 # Taking multiple screenshots in succession causes `launchd` to trigger the same
 # amount of times. Checking for this lock in the `if` statement above ensures
 # that only the first instance of the script executes the rest of the script
 # body.
-trap 'rm -rf "$LOCK"' EXIT
+{ trap 'rm -rf "$LOCK"' EXIT && mkdir "$LOCK" 2>/dev/null } || exit 1
 
 sleep $EXECUTION_DELAY # Give time for all screenshots to be written to disk
 
