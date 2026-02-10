a84977be42b17d9436b94f0c0c51118bab1adbdd
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-02-10 00:10:37 -0800

    Use pipe, no grep

--- src/_sst.zsh
+++ src/_sst.zsh
@@ -14,25 +14,16 @@ _sst() {
     -@ "${ARG_FILES_DIR}/screenshot.args"
   )
 
+  "$EXIFTOOL" "${(@)exiftool_args}" &>>!"$EXIFTOOL_LOG" &
+  integer -r et_pid=$!
+  _cmc_log DEBUG "Started ExifTool in the backround (PID: ${et_pid})"
+
+  exec {pending_fd}>>!"$PENDING_LIST"
+  exec {processed_fd}>>!"$PROCESSED_LIST"
+
+  print -l -u $pending_fd -- '-echo2' 'ExifTool STARTED'
+
   {
-    "$EXIFTOOL" "${(@)exiftool_args}" >>!"$EXIFTOOL_LOG" 2>&1 &
-    integer -r et_pid=$!
-
-    local message="${SERVICE_NAME} BEGIN"
-    print -l -- -echo4 "$message" -execute >!"$PENDING_LIST"
-
-    integer timeout=60
-    while ! grep -q -e "$message" "$EXIFTOOL_LOG" &>/dev/null; do
-      if (( timeout-- <= 0 )); then
-        # return 69: BSD EX_UNAVAILABLE
-        _cmc_err 69 "ExifTool could not be ready in time"
-      fi
-
-      _cmc_log DEBUG 'Waiting for ExifTool to be ready...'
-      sleep 0.1
-    done
-    _cmc_log INFO "ExifTool:${et_pid} started"
-
     integer count=0
     if ! while IFS= read -r -t -u 0 file; do
       if [[ -z $file ]]; then
@@ -41,27 +32,20 @@ _sst() {
       fi
 
       _cmc_log INFO "Submitting: '${file:t}'"
-      print -l -- "$file" '-execute' >>!"$PENDING_LIST"
+      print -l -u $pending_fd -- "$file" '-execute'
+      print -u $processed_fd -- "${file:t}"
       (( ++count ))
     done; then
       # return 66: BSD EX_NOINPUT
-      _cmc_err 66 "No screenshots to process: '${INPUT_DIR}/'"
+      _cmc_err 66 "No screenshots to process: '${INPUT_DIR:A}/'"
     fi
-
-    message="${SERVICE_NAME} END"
-    print -l -- -echo4 "$message" -execute >>!"$PENDING_LIST"
-    while ! grep -q -e "$message" "$EXIFTOOL_LOG" &>/dev/null; do
-      _cmc_log DEBUG 'Waiting for ExifTool to finish processing...'
-      sleep 0.1
-    done
-    print -l -- '-stay_open' 'False' >>!"$PENDING_LIST"
+    print -l -u $pending_fd -- '-echo3' 'ExifTool Finished' '-stay_open' 'False' '-echo3' 'pahabol'
 
     local unit=screenshot
     if (( count > 1 )); then
       unit+=s
     fi
-    _cmc_log INFO "Submitted ${count} ${unit} to ExifTool:${et_pid}"
-    grep -q "$INPUT_DIR" "$PENDING_LIST" >! "${TMPDIR}/processed.txt"
+    _cmc_log INFO "Submitted ${count} ${unit} to ExifTool (PID:${et_pid})"
 
     local -r archive_name="Screenshots_${current_month}.aar"
     local aa_cmd=archive
@@ -71,11 +55,16 @@ _sst() {
 
     _cmc_log INFO 'Archiving original files...'
     "$AA" $aa_cmd -v -a lz4 -d "$INPUT_DIR" -o "${OUTPUT_DIR}/${archive_name}"\
-      -include-path-list "${TMPDIR}/processed.txt" &>!"$AA_LOG" || \
+      -include-path-list "$PROCESSED_LIST" &>>!"$AA_LOG" || \
       _cmc_err 73 "Apple Archive Failed: ${(j: ⏎ :)${(f)mapfile[$AA_LOG]}}"
       # return 73: BSD EX_CANTCREAT
+
+    wait $et_pid || true
   } always {
     integer -r status_code=$?
+
+    exec {pending_fd}>&-
+    exec {processed_fd}>&-
 
     _cmc_log DEBUG "Status Code: ${status_code}"
 
