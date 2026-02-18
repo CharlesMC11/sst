5f9dd0127e527626c707a3bbfb71660731f42c81
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-02-17 22:49:25 -0800

    Use scratch registers

--- src/native/is_image.s
+++ src/native/is_image.s
@@ -25,48 +25,48 @@ _is_image:
     cmp     x0, #12                     // Check if at least 12 bytes were read
     b.lt   .L_false
 
-    ldr     q0, [sp, #16]               // Load the file header into SIMD lane
+    ldr     x9, [sp, #16]
+    ldr     w10, [sp, #24]
 
     // PNG: 89 50 4E 47 0D 0A 1A 0A
-    movz    x1, #0x5089
-    movk    x1, #0x474E, lsl #16
-    movk    x1, #0x0A0D, lsl #32
-    movk    x1, #0x0A1A, lsl #48
-    fmov    d1, x1                      // Move 64-bit pattern to SIMD lane
-    cmeq    v1.2d, v0.2d, v1.2d         // If bits match, lane becomes all 1s
-
-    // HEIF: 'ftypheic' or 'ftypmif1' at offset 4
-    ext     v2.16b, v0.16b, v0.16b, #4
-    movz    x2, #0x7466                 // 'ft'
-    movk    x2, #0x7079, lsl #16        // 'yp'
-    movk    x2, #0x6568, lsl #32        // 'he'
-    movk    x2, #0x6369, lsl #48        // 'ic'
-    fmov    d2, x2
-    cmeq    v2.2d, v1.2d, v2.2d
-
-    orr     v1.16b, v1.16b, v2.16b
-
-    fmov    x1, d1
-    cbnz    x1, .L_true
+    movz    x11, #0x5089
+    movk    x11, #0x474E, lsl #16
+    movk    x11, #0x0A0D, lsl #32
+    movk    x11, #0x0A1A, lsl #48
+    cmp     x9, x11
+    b.eq    .L_true
 
     // JPEG: FF D8 FF
-    fmov    w1, s0
-    and     w1, w1, #0xFFFFFF
-    mov     w2, #0xD8FF
-    movk    w2, #0x00FF, lsl #16
-    cmp     w1, w2
+    ubfx    w11, w9, #0, #24
+    movz    w12, #0xD8FF
+    movk    w12, #0x00FF, lsl #16
+    cmp     w11, w12
     b.eq    .L_true
 
     // TIFF: 'II' (49 49 2A 00) or 'MM'(4D 4D 00 2A)
-    fmov    w1, s0
-    mov     w2, #0x4949                 // 'II'
-    movk    w2, #0x002A, lsl #16
-    cmp     w1, w2
+    ubfx    w11, w9, #0, #32
+    movz    w12, #0x4949                // 'II'
+    movk    w12, #0x002A, lsl #16
+    cmp     w11, w12
     b.eq    .L_true
 
-    mov     w2, #0x4D4D                 // 'MM'
-    movk    w2, #0x2A00, lsl #16
-    cmp     w1, w2
+    movz    w12, #0x4D4D                // 'MM'
+    movk    w12, #0x2A00, lsl #16
+    cmp     w11, w12
+    b.eq    .L_true
+
+    // HEIF: 'ftypmif1' or 'ftypheic' at offset 4
+    extr    x11, x10, x9, #32
+    movz    x12, #0x7466                // 'ft'
+    movk    x12, #0x7079, lsl #16       // 'yp'
+    movk    x12, #0x696D, lsl #32       // 'mi'
+    movk    x12, #0x3166, lsl #48       // 'f1'
+    cmp     x11, x12
+    b.eq    .L_true
+
+    movk    x12, #0x6568, lsl #32       // 'he'
+    movk    x12, #0x6369, lsl #48       // 'ic'
+    cmp     x11, x12
     b.eq    .L_true
 
 .L_false:
