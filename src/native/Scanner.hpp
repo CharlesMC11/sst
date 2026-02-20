0b0f201cd468db93af2b2cf3e85301ad0e592c79
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-02-20 03:42:00 -0800

    Add documentation

--- src/native/Scanner.hpp
+++ src/native/Scanner.hpp
@@ -9,6 +9,13 @@ namespace sst::scanner {
 
 using ScanResult = std::expected<std::vector<std::string>, uint32_t>;
 
+/*!
+ * @param dirname
+ * The directory to collect images from
+ *
+ * @returns
+ *
+ */
 ScanResult collect_images(const char dirname[]);
 
 }  // namespace sst::scanner
--- src/native/Signatures.h
+++ src/native/Signatures.h
@@ -8,6 +8,15 @@ namespace sst::signatures {
 extern "C" {
 #endif
 
+/*!
+ * Check if a given array of bytes matches an image's magic pattern
+ *
+ * @param buffer
+ * The bytes to check
+ *
+ * @returns
+ * `true` if `buffer` contains magic bytes from common image formats
+ */
 bool is_image(uint8_t buffer[]);
 
 #ifdef __cplusplus
